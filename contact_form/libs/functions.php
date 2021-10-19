da3a4f93d0cd8e618b1af17bbab1da6f2bc6675b
ttt-tomy <aa00.van@outlook.jp>
2021-10-19 17:09:32 +0900

    Operation test of mail filter.

--- contact_form/libs/functions.php
+++ contact_form/libs/functions.php
@@ -10,21 +10,30 @@ function h($var) {
 }
 //入力値に不正なデータがないかなどをチェックする関数
 function checkInput($var){
+  //変数が配列であるか判定
   if(is_array($var)){
+    //変数をバラし、それぞれにcheckInput関数処理
     return array_map('checkInput', $var);
+  //バラしたからこっちが走る。
   }else{
     //NULLバイト攻撃対策
     if(preg_match('/\0/', $var)){
+      //スクリプトの終了（メッセージあり）
       die('不正な文字が含まれています。');
-    }
+    }//else echo 'フィルター通過<br>'; //動作テスト用
+          //動作テスト
+          //if(preg_match('/yyy/', $var)){
+          //  die('テストyyy不通');
+          //}else echo 'テストyyy通過<br>'; //動作テスト用
     //文字エンコードのチェック
     if(!mb_check_encoding($var, 'UTF-8')){ 
       die('不正なエンコードです。');
-    }
+    }//else echo 'フィルター通過<br>'; //動作テスト用
     //改行、タブ以外の制御文字のチェック
     if(preg_match('/\A[\r\n\t[:^cntrl:]]*\z/u', $var) === 0){  
       die('不正な入力です。制御文字は使用できません。');
-    }
+    }//else echo 'フィルター通過<br>'; //動作テスト用
+    //変数を返す（誤動作抑止）
     return $var;
   }
 }
