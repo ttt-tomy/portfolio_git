0c007df25d99a1e14e739d33cb194b9eed9e148e
ttt-tomy <92297838+ttt-tomy@users.noreply.github.com>
2021-10-28 17:45:02 +0900

    C-language test.

--- test-C/new-test/test.c
+++ test-C/new-test/test.c
@@ -2,28 +2,36 @@
 
 //標準入出力を利用する為のヘッダファイル(ライブラリ)読み込み
 #include <stdio.h>
-//三角関数など数学系を利用する為のヘッダファイル(ライブラリ)読み込み
-//ライブラリ変数にM_PI(π)がいる模様
-//コンパイル時にライブラリリンクオプションが必要
-#include <math.h>
+#include <string.h>
 
 int main (void){ //main:メイン関数の意味らしい void:空の意味(引数)
 	
-	printf("%f\n",sin(0));
-	//sin関数の返り値はdouble型、引数はラジアン(弧度法)で指定する
-	//%fは実数、倍精度実数の%lfでも良いような気がする（処理速度の問題だろうか)
-	
-	int i ;
-	
-	printf("%s","M_PI(%f指定)は、");
-	printf("%f\n",M_PI);
-	printf("%s","M_PI(%lf指定)は、");
-	printf("%lf\n",M_PI);
-	
-	for(i=0;i<91;i++){
-		printf("%2d° %f\n",i,sin(i*M_PI/180));
-		//%2dの2は二桁桁枠指定の2
+	void drawLine1(int length){
+		int i ;
+		
+		for(i= 0;i<length+2;i++){
+			printf("+");
+		}
+		printf("\n");
 	}
+	
+	
+	void drawLine2(char str[]){
+		printf("+");
+		printf("%s",str);
+		printf("+\n");
+	}
+	
+	char str[128];
+	int length;
+
+	printf("文字列を入力してください");
+	scanf("%s",str);
+	length = strlen(str);
+	
+	drawLine1(length);
+	drawLine2(str);
+	drawLine1(length);
 	
 	return(0);
 	
@@ -31,7 +39,7 @@ int main (void){ //main:メイン関数の意味らしい void:空の意味(引�
 
 /*
 実行結果
-0.000000
+
 */
 
 /*
