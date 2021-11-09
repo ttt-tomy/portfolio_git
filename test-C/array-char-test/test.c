5b0520e648de98e69a50fef28e4b5b652ae59ff1
ttt-tomy <92297838+ttt-tomy@users.noreply.github.com>
2021-11-09 20:05:36 +0900

    Array char is each 1 character by 1 character.

---
+++ test-C/array-char-test/test.c
@@ -0,0 +1,36 @@
+/* test.c */
+
+#include <stdio.h>
+
+int main (void){
+	
+	char org_str[] = "apple";
+	char copy_str[128] = {0};//配列の初期化
+	
+	//null含めて1文字ずつ入ってる
+	copy_str[0] = org_str[0];
+	copy_str[1] = org_str[1];
+	copy_str[2] = org_str[2];
+	copy_str[3] = org_str[3];
+	
+	printf("original string %s\n",org_str);
+	printf("copy string %s\n",copy_str);
+	
+	return 0 ;
+}
+
+/*実行結果
+original string apple
+copy string apple
+*/
+
+/*
+参考
+フォーマット指定子とデータ型の一覧
+https://k-cube.co.jp/wakaba/server/format.html
+ヘッダファイル(ライブラリ)一覧
+https://k-cube.co.jp/wakaba/server/head_top.html
+ライブラリのリンクを忘れずに(コンパイル時) ※今回は-lm(link math)
+https://qiita.com/percipere/items/678a8ec1299a7564f6a3
+*/
+
