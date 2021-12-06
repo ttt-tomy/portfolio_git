94e2f37186205a32bd6605efbdb798668e0e5c98
ttt-tomy <92297838+ttt-tomy@users.noreply.github.com>
2021-12-06 22:35:33 +0900

    Add practice files.

--- test-C/practice/prac0706.c
+++ test-C/practice/prac0706.c
@@ -5,18 +5,24 @@
 #include <stdio.h>
 
 int factorial(int n){
+	int i,anser;
 	
-}
-
-int main (void){
-	int n,i;
-	printf("数値を入力してください\n階乗を計算します。\n");
-	scanf("%d",&n);
-	
-	int anser = n;
+	anser = n;
 	for(i=0;i<n-1;i++){
 		anser = anser*(n-i-1);
 	}
+	
+	return anser ;
+}
+
+int main (void){
+	int n,anser;
+	
+	printf("数値を入力してください\n階乗を計算します。\n");
+	scanf("%d",&n);
+	
+	anser = factorial(n);
+	
 	printf("階乗後の値は%dです\n",anser);
 	
 	return 0;
